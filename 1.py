b5ffafbf9c3b9909bd99d3afd7e0ea5407c7ed7e
Himanshu <himanshumohannikam@gmail.com>
2024-06-04 13:00:11 +0200

    commit 2

--- 1.py
+++ 1.py
@@ -1,10 +1,30 @@
-print("Hello")
-print("Himanshu commit")
-print ("commit 2")
-a = 12
-if a>10:
-    print("a is greater than 10")
-    a+=1
-else:
-    print("a is less than 10")
-print ("a is greater than 10")
+from mesa import Agent, Model
+from mesa.time import RandomActivation
+from mesa.space import MultiGrid
+import random
+from mesa.visualization.ModularVisualization import ModularServer
+from mesa.visualization.modules import CanvasGrid, ChartModule
+
+
+class Labyrinth(Model):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.width = width
+        self.height = height
+        self.schedule = RandomActivation(self)
+        self.grid = MultiGrid(self.width, self.height, torus=True)
+
+
+
+
+def Sim_portrayal(agent):
+    if agent is None:
+        return
+    portrayal = {}
+
+
+canvas_element = CanvasGrid(Sim_portrayal, 20, 20, 500, 500)
+
+
+server = ModularServer(Labyrinth, [canvas_element], "Simulation Visualization", {"width":10,"height":10})
+server.launch()
