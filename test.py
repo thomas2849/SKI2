69ae184b35d2630e235bc743f49d04721da408ea
Himanshu <himanshumohannikam@gmail.com>
2024-06-05 14:09:44 +0200

    Implemented BFS Algorithm in Labyrinth

--- test.py
+++ test.py
@@ -2,27 +2,25 @@ from mesa import Agent, Model
 from mesa.time import RandomActivation
 from mesa.space import MultiGrid
 from mesa.visualization.ModularVisualization import ModularServer
-from mesa.visualization.modules import CanvasGrid, ChartModule
+from mesa.visualization.modules import CanvasGrid
 import numpy as np
 import random
+from queue import Queue
 
 class PathFinder(Agent):
-    def __init__(self, unique_id, model, found=False):
+    def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        self.found = found
+        self.path = []
+        self.path_index = 0
 
     def move(self):
-        possible_steps = self.model.grid.get_neighborhood(
-            self.pos,
-            moore=True,)
-        new_position = self.random.choice(possible_steps)
-        self.model.grid.move_agent(self, new_position)
+        if self.path_index < len(self.path):
+            new_position = self.path[self.path_index]
+            self.model.grid.move_agent(self, new_position)
+            self.path_index += 1
 
     def step(self):
         self.move()
-        if self.pos == self.model.destination:
-            self.found = True
-            self.model.running = False
 
 class Blockage(Agent):
     def __init__(self, unique_id, model):
@@ -37,8 +35,10 @@ class Labyrinth(Model):
         self.grid = MultiGrid(self.width, self.height, torus=False)
         self.maze = self.create_maze(dim)
         self.start = (1, 0)
-        self.destination = (2*dim-1, 2*dim)
+        self.destination = (2 * dim - 1, 2 * dim - 1)
+        self.pathfinder = PathFinder(self.next_id(), self)
         self.place_agents()
+        self.move_agents()
 
     def step(self):
         self.schedule.step()
@@ -78,16 +78,38 @@ class Labyrinth(Model):
     def place_agents(self):
         for (x, y), value in np.ndenumerate(self.maze):
             if value == 1:
-                blockage = Blockage((x, y), self)
+                blockage = Blockage(self.next_id(), self)
                 self.grid.place_agent(blockage, (x, y))
             elif (x, y) == self.start:
-                pathfinder = PathFinder((x, y), self)
-                self.grid.place_agent(pathfinder, (x, y))
-                self.schedule.add(pathfinder)
-            '''elif (x, y) == self.destination:
-                # Just to mark the destination
-                blockage = Blockage((x, y), self)
-                self.grid.place_agent(blockage, (x, y))'''
+                self.grid.place_agent(self.pathfinder, (x, y))
+                self.schedule.add(self.pathfinder)
+
+    def move_agents(self):
+        path = self.find_path(self.maze)
+        self.pathfinder.path = path
+
+    def find_path(self, maze):
+        # BFS algorithm to find the shortest path
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        start = (1, 0)
+        end = (maze.shape[0] - 2, maze.shape[1] - 2)
+        visited = np.zeros_like(maze, dtype=bool)
+        visited[start] = True
+        queue = Queue()
+        queue.put((start, []))
+        while not queue.empty():
+            (node, path) = queue.get()
+            for dx, dy in directions:
+                next_node = (node[0] + dx, node[1] + dy)
+                if next_node == end:
+                    return path + [next_node]
+                if (0 <= next_node[0] < maze.shape[0] and
+                    0 <= next_node[1] < maze.shape[1] and
+                    maze[next_node] == 0 and
+                    not visited[next_node]):
+                    visited[next_node] = True
+                    queue.put((next_node, path + [next_node]))
+        return []
 
 def agent_portrayal(agent):
     if isinstance(agent, PathFinder):
