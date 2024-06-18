568f77a9586a4eaae66a5f744f848b0b18407018
Thomas Antony <thommy2849@gmail.com>
2024-06-18 12:34:31 +0200

    graph

--- test.py
+++ test.py
@@ -168,7 +168,7 @@ class Labyrinth(Model):
         while len(stack) > 0:
             x, y = stack[-1]
             directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-            random.shuffle(directions)
+            #random.shuffle(directions)
             for dx, dy in directions:
                 nx, ny = x + dx, y + dy
                 if nx >= 0 and ny >= 0 and nx < dim and ny < dim and maze[2 * nx + 1, 2 * ny + 1] == 1:
